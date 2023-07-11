3d8aaf6d670d84e0c14a018ae229dc48786440ce
Harsh Raj <95171067+Harshsaxena18@users.noreply.github.com>
2023-07-12 02:10:58 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,15 @@
+def greedy_search(photo, model, max_length, ixtoword):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([photo, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = ixtoword[yhat]
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
