5bdf79ae9c70573a5775b2a5a3ea6656eb6cbc8e
Harsh Raj <95171067+Harshsaxena18@users.noreply.github.com>
2023-07-13 04:59:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,36 @@
 import streamlit as st
 from PIL import Image
+from io import BytesIO
+from image_captioning import generate_captions
+import requests
 import numpy as np
-from data_loader import load_descriptions, create_tokenizer, load_train_images, load_image_paths, load_train_descriptions, create_vocab, create_word_mappings, create_sequences, create_data_generator
-from model import create_model
-from inference import greedy_search
 
-
-
-
-
-# Set up constants
-token_path = "/content/Flickr8k.token.txt"
-train_images_path = '/content/Flickr_8k.trainImages.txt'
-test_images_path = '/content/Flickr_8k.testImages.txt'
-images_path = '/content/Flicker8k_Dataset/'
-glove_path = '/content'
-
-# Load descriptions
-descriptions = load_descriptions(token_path)
-
-# Create tokenizer
-tokenizer = create_tokenizer(descriptions)
-
-# Load train images
-train_images = load_train_images(train_images_path)
-
-# Load image paths
-train_img = load_image_paths(images_path, train_images)
-
-# Load train descriptions
-train_descriptions, all_train_captions = load_train_descriptions(tokenizer, train_images)
-
-# Create vocabulary
-vocab = create_vocab(all_train_captions)
-
-# Create word mappings
-wordtoix, ixtoword, vocab_size = create_word_mappings(vocab)
-
-# Create sequences
-sequences = create_sequences(train_descriptions, wordtoix, max_length)
-
-# Create data generator
-data_generator = create_data_generator(sequences, train_features, wordtoix, max_length, vocab_size, batch_size)
-
-# Create model
-model = create_model(vocab_size, max_length, embedding_matrix)
-
-# Load the trained model weights
-model.load_weights('saved_models/Final_Image_Captioning.h5')
-
-
-# Streamlit app
 def main():
-    st.title("Image Captioning")
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.write("")
-        st.write("Generating caption...")
-        # Preprocess the image and encode it
-        # ...
-        # Generate the caption using the model
-        caption = greedy_search(encoded_image, model, max_length, ixtoword)
-        st.write("Caption:", caption)
+    st.title("Image Captioning App")
+    st.write("Upload an image or enter the URL of an image.")
+
+    # Image upload section
+    upload_option = st.radio("Select an option", ("Upload Image", "Image URL"))
+
+    if upload_option == "Upload Image":
+        uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    else:
+        image_url = st.text_input("Enter the URL of an image")
+        if image_url:
+            try:
+                response = requests.get(image_url)
+                image = Image.open(BytesIO(response.content))
+                st.image(image, caption="Image from URL", use_column_width=True)
+            except:
+                st.error("Invalid image URL. Please enter a valid URL.")
+
+    if uploaded_file or image_url:
+        captions = generate_captions(image)
+        # Display the generated captions in the web interface
 
 if __name__ == '__main__':
     main()
